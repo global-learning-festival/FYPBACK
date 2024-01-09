a6ad873153910acbd18b8cf1fdbecc344c7ae497
Caden Cheong <ccccadenn@gmail.com>
2024-01-09 18:16:33 +0800

    important info app.js #7

--- model/importantInfo.js
+++ model/importantInfo.js
@@ -21,7 +21,7 @@ const importantInformation = {
     },
 
     getImportantInfomation: function (callback) {
-        return query(`SELECT title, subtitle, description FROM importantinfo`).then((result,err) =>{
+        return query(`SELECT infoid, title, subtitle, description FROM importantinfo`).then((result,err) =>{
     
             if (err) {
                 callback(err, null);
@@ -33,6 +33,56 @@ const importantInformation = {
             
         });
     },
+    getImportantInformationById: function (infoid, callback) {
+        return query('SELECT * FROM importantinfo WHERE infoid = $1', [infoid])
+            .then((result, err) => {
+                if (err) {
+                    callback(err, null);
+                    console.log(err);
+                    return;
+                } else {
+                    callback(null, result);
+                }
+            });
+    },
+    
+        updateImportantInformation: function (infoid, title, subtitle, description, callback) {
+        return query(
+          'UPDATE importantinfo SET title = $2, subtitle = $3, description = $4 WHERE infoid = $1 RETURNING *',
+          [infoid, title, subtitle, description]
+        )
+          .then((result, err) => {
+            if (err) {
+              callback(err, null);
+              console.log(err);
+              return;
+            } else {
+              return callback(null, result);
+            }
+          })
+          .catch((error) => {
+            console.error('Error updating important information:', error);
+            return callback(error, null);
+          });
+      },
+      
+      deleteImportantInformation: function (infoid, callback) {
+        return query(`DELETE FROM importantinfo
+        WHERE infoid=$1`,
+        [infoid])
+        .then((result,err) =>{
+    
+            if (err) {
+                callback(err, null);
+                return;
+            }
+            else  {
+                return callback(null, result);
+            } 
+            
+        });
+    },
+    
 }
            
 module.exports = importantInformation;
