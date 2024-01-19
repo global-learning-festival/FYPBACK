0ad7cfe9487d38f9b9026ad2722c802502b3435b
DylanFromSP <92902711+DylanFromSP@users.noreply.github.com>
2024-01-19 14:46:40 +0800

    Fixing Comma Placement Announcement #3

--- model/announcement.js
+++ model/announcement.js
@@ -22,7 +22,7 @@ const announcement = {
     },
 
     getAnnouncements: function(callback) {
-        return query(`SELECT announcementid, eventid, title, description, TO_CHAR(created_at, 'DD/MM/YYYY ,HH12:MIam') AS "created_on", TO_CHAR(updated_at, 'DD/MM/YYYY ,HH12:MIam') AS "updated_on" FROM announcements ORDER BY updated_at DESC;`).then((result,err) =>{
+        return query(`SELECT announcementid, eventid, title, description, TO_CHAR(created_at, 'DD/MM/YYYY, HH12:MIam') AS "created_on", TO_CHAR(updated_at, 'DD/MM/YYYY, HH12:MIam') AS "updated_on" FROM announcements ORDER BY updated_at DESC;`).then((result,err) =>{
     
             if (err) {
                 callback(err, null);
@@ -35,7 +35,7 @@ const announcement = {
         });
     },
     getAnnouncementById: function(announcementid, callback) {
-        return query(`SELECT announcementid,eventid, title, description, image, TO_CHAR(created_at, 'DD/MM/YYYY ,HH12:MIam') AS "created_on", TO_CHAR(updated_at, 'DD/MM/YYYY ,HH12:MIam') AS "updated_on" FROM announcements WHERE announcementid = $1;`, [announcementid]).then((result,err) =>{
+        return query(`SELECT announcementid,eventid, title, description, image, TO_CHAR(created_at, 'DD/MM/YYYY, HH12:MIam') AS "created_on", TO_CHAR(updated_at, 'DD/MM/YYYY, HH12:MIam') AS "updated_on" FROM announcements WHERE announcementid = $1;`, [announcementid]).then((result,err) =>{
     
             if (err) {
                 callback(err, null);
@@ -47,8 +47,8 @@ const announcement = {
             
         });
     },
-    updateAnnouncement: function(announcementid, title, description, image,eventid, callback) {
-        return query(`UPDATE announcements SET title = $1, description = $2, image = $3,eventid = $4 WHERE announcementid = $5 RETURNING *`, [title, description, image,eventid, announcementid])
+    updateAnnouncement: function(announcementid, title, description, image, eventid, callback) {
+        return query(`UPDATE announcements SET title = $1, description = $2, image = $3,eventid = $4 WHERE announcementid = $5 RETURNING *`, [title, description, image, eventid, announcementid])
 
             .then((result, err) => {
                 if (err) {
@@ -88,7 +88,7 @@ const announcement = {
 
     getAnnouncementsByEventId: function (eventid, callback) {
     return query(
-        `SELECT announcementid, eventid, title, description, TO_CHAR(created_at, 'DD/MM/YYYY ,HH12:MIam') AS "created_on", TO_CHAR(updated_at, 'DD/MM/YYYY ,HH12:MIam') AS "updated_on" FROM announcements WHERE eventid = $1 ORDER BY updated_at DESC;`,
+        `SELECT announcementid, eventid, title, description, TO_CHAR(created_at, 'DD/MM/YYYY, HH12:MIam') AS "created_on", TO_CHAR(updated_at, 'DD/MM/YYYY, HH12:MIam') AS "updated_on" FROM announcements WHERE eventid = $1 ORDER BY updated_at DESC;`,
         [eventid]
     ).then((result, err) => {
         if (err) {
