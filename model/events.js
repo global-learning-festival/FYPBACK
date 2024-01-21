e9083acbe446d53a3327780d6162658e570d4ae1
Caden Cheong <ccccadenn@gmail.com>
2024-01-21 14:27:41 +0800

    eventsfixtiming #2

--- model/events.js
+++ model/events.js
@@ -32,10 +32,13 @@ const events = {
             // Convert datetime-local strings to the required format
             const formattedStartTime = formatDateTime(time_start);
             const formattedEndTime = formatDateTime(time_end);
+            console.log('Formatted Start Time:', formattedStartTime);
+            console.log('Formatted End Time:', formattedEndTime);
 
             return query(`
                 INSERT INTO events (title, image_banner, time_start, time_end, location, keynote_speaker, description, survey_link)
-                VALUES ($1, $2, TO_TIMESTAMP($3, 'YYYY-MM-DD HH24:MI:SS') AT TIME ZONE 'Asia/Singapore', TO_TIMESTAMP($4, 'YYYY-MM-DD HH24:MI:SS'), $5, $6, $7, $8)
+                VALUES ($1, $2,TO_TIMESTAMP($3, 'YYYY-MM-DD HH24:MI:SS') AT TIME ZONE 'Asia/Singapore',
+                TO_TIMESTAMP($4, 'YYYY-MM-DD HH24:MI:SS') AT TIME ZONE 'Asia/Singapore', $5, $6, $7, $8)
                 RETURNING *
             `, [title, image_banner, formattedStartTime, formattedEndTime, location, keynote_speaker, description, survey_link])
                 .then((result) => {
@@ -73,63 +76,63 @@ const events = {
     },
     //this function gets everything but time format in UTC
     getEventbyId: function (eventid, callback) {
-       return query(`SELECT eventid, title, image_banner,
-       TO_CHAR(time_start AT TIME ZONE \'Asia/Singapore\', 'YYYY-MM-DD HH24:MI:SS' ) AS "time_start",
-       TO_CHAR(time_end AT TIME ZONE \'Asia/Singapore\', 'YYYY-MM-DD HH24:MI:SS' ) AS "time_end",
+        return query(`SELECT eventid, title, image_banner,
+       TO_CHAR(time_start , 'YYYY-MM-DD HH24:MI:SS' ) AS "time_start",
+       TO_CHAR(time_end , 'YYYY-MM-DD HH24:MI:SS' ) AS "time_end",
        location, keynote_speaker, description, survey_link
        FROM events WHERE eventid = $1`, [eventid])
-      
-       .then((result, err) => {
-               if (err) {
+
+            .then((result, err) => {
+                if (err) {
                     callback(err, null);
                     console.log(err);
                     return;
                 } else {
-                   console.log(result);
+                    console.log(result);
                     callback(null, result);
-               }
+                }
             });
-   },
-   // this function status 200 but gets nothing in postman
-//    getEventbyId: function (eventid, callback) {
-//     const formatDateTime = (datetime) => {
-//         if (datetime === undefined || datetime === null) {
-//             return null;
-//         }
+    },
+    // this function status 200 but gets nothing in postman
+    //    getEventbyId: function (eventid, callback) {
+    //     const formatDateTime = (datetime) => {
+    //         if (datetime === undefined || datetime === null) {
+    //             return null;
+    //         }
 
-//         const parsedDateTime = new Date(datetime);
+    //         const parsedDateTime = new Date(datetime);
 
-//         if (isNaN(parsedDateTime.getTime())) {
-//             throw new Error('Invalid datetime string: ' + datetime);
-//         }
+    //         if (isNaN(parsedDateTime.getTime())) {
+    //             throw new Error('Invalid datetime string: ' + datetime);
+    //         }
 
-//         // Format the datetime to a string in the desired format
-//         const formattedDateTime = parsedDateTime.toISOString().replace('T', ' ').replace('Z', '').slice(0, -5);
+    //         // Format the datetime to a string in the desired format
+    //         const formattedDateTime = parsedDateTime.toISOString().replace('T', ' ').replace('Z', '').slice(0, -5);
 
-//         return formattedDateTime;
-//     };
+    //         return formattedDateTime;
+    //     };
 
-//     const queryText = 'SELECT *, time_start AT TIME ZONE \'Asia/Singapore\' AS time_start, time_end AT TIME ZONE \'Asia/Singapore\' AS time_end FROM events WHERE eventid = $1';
+    //     const queryText = 'SELECT *, time_start AT TIME ZONE \'Asia/Singapore\' AS time_start, time_end AT TIME ZONE \'Asia/Singapore\' AS time_end FROM events WHERE eventid = $1';
 
-//     query(queryText, [eventid])
-//         .then((result) => {
-//             // Format the datetime values in the result
-//             const formattedResult = result.rows.map(row => ({
-//                 ...row,
-//                 formatted_time_start: formatDateTime(row.time_start),
-//                 formatted_time_end: formatDateTime(row.time_end)
-//             }));
+    //     query(queryText, [eventid])
+    //         .then((result) => {
+    //             // Format the datetime values in the result
+    //             const formattedResult = result.rows.map(row => ({
+    //                 ...row,
+    //                 formatted_time_start: formatDateTime(row.time_start),
+    //                 formatted_time_end: formatDateTime(row.time_end)
+    //             }));
 
-//             console.log(formattedResult);
-//             callback(null, formattedResult);
-//         })
-//         .catch((error) => {
-//             console.error('Error executing query:', error);
-//             callback(error, null); // Pass the error to the callback
-//         });
-// },
+    //             console.log(formattedResult);
+    //             callback(null, formattedResult);
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error executing query:', error);
+    //             callback(error, null); // Pass the error to the callback
+    //         });
+    // },
 
-    
+
     updateEvent: function (eventid, title, image_banner, time_start, time_end, location, keynote_speaker, description, survey_link, callback) {
         if (typeof callback !== 'function') {
             console.error('Callback is not a function.');
@@ -189,16 +192,16 @@ const events = {
             .then((result, err) => {
 
                 if (err) {
-                        callback(err, null);
-                        return;
-                    }
-                    else {
-                        return callback(null, result);
-                    }
+                    callback(err, null);
+                    return;
+                }
+                else {
+                    return callback(null, result);
+                }
 
-                });
-            },
-            
+            });
+    },
+
 }
 
 module.exports = events;
