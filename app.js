2665f43996fbee08363e3ce3b7fbca9b43fc6a88
JaydenChowJW <jaydenchowjw@gmail.com>
2024-01-26 00:32:47 +0800

    changes to validate login to send data

--- app.js
+++ app.js
@@ -26,6 +26,37 @@ const upload = multer({ storage: storage });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.get('/validateLogin', (req, res, next) => {
+    //If the token is valid, the logic extracts the user id and the role information.
+    //If the role is not user, then response 403 UnAuthorized
+    //The user id information is inserted into the request.body.userId
+        console.log('http header - user ', req.headers['user']);
+        if (typeof req.headers.authorization !== "undefined") {
+            // Retrieve the authorization header and parse out the
+            // JWT using the split function
+            let token = req.headers.authorization.split(' ')[1];
+            //console.log('Check for received token from frontend : \n');
+            //console.log(token);
+            jwt.verify(token, process.env.JWTKey, (err, data) => {
+                console.log('data extracted from token \n',data);
+                if (err) {
+                    console.log(err)
+                    return res.status(200).json({ message: 'Unauthorized access' });
+                }   
+                else {
+                    req.body.userId = data.id;
+                    req.body.role = data.role;
+                    return res.status(200).json(data);
+
+                }
+            })
+  
+      }else{
+        res.status(403).send({ message: 'Unauthorized access' });
+
+      }
+    } //End of checkForValidUserRoleUser
+)
 
 //Test
 app.post('/product', (req, res) => {
@@ -548,6 +579,7 @@ app.post('/login', (req, res) => {
 
 
 })
+
 app.post('/register', (req, res) => {
 
     console.log('processRegister running.');
@@ -752,36 +784,8 @@ app.put('/user/:userid', (req, res) => {
 });
 
 
-app.get('/validateLogin', (req, res, next) => {
-    //If the token is valid, the logic extracts the user id and the role information.
-    //If the role is not user, then response 403 UnAuthorized
-    //The user id information is inserted into the request.body.userId
-        console.log('http header - user ', req.headers['user']);
-        if (typeof req.headers.authorization !== "undefined") {
-            // Retrieve the authorization header and parse out the
-            // JWT using the split function
-            let token = req.headers.authorization.split(' ')[1];
-            //console.log('Check for received token from frontend : \n');
-            //console.log(token);
-            jwt.verify(token, process.env.JWTKey, (err, data) => {
-                console.log('data extracted from token \n',data);
-                if (err) {
-                    console.log(err)
-                    return res.status(200).json({ message: 'Unauthorized access' });
-                }   
-                else {
-                    req.body.userId = data.id;
-                    req.body.role = data.role;
-                    next();
-                }
-            })
-  
-      }else{
-        res.status(403).send({ message: 'Unauthorized access' });
 
-      }
-    } //End of checkForValidUserRoleUser
-)
 
 
 module.exports = app;
+
