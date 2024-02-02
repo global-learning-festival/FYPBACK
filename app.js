354363cf05e6b47ddb97b161ed1d3b08b2f89c3e
JaydenChowJW <jaydenchowjw@gmail.com>
2024-02-02 20:23:10 +0800

    netlifyurl

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "http://localhost:3001"
+    "https://main--jade-brigadeiros-e6f0a2.netlify.app"
     );
   res.setHeader(
     "Access-Control-Allow-Methods",
