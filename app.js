d56693e11f17c709c9228aa5d74e6c76a8958c75
JaydenChowJW <jaydenchowjw@gmail.com>
2024-02-02 23:43:18 +0800

    multiurltest

--- app.js
+++ app.js
@@ -27,10 +27,18 @@ app.use(bodyParser.json());
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://ilfadmin.netlify.app"
-    );
+  const allowedOrigins = [
+    "https://ilfadmin.netlify.app",
+    "https://ilffront.netlify.app",
+    // Add more domains as needed
+  ];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
@@ -41,14 +49,11 @@ app.use((req, res, next) => {
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
   res.setHeader("Access-Control-Max-Age", 7200);
 
   next();
 });
-app.get(
-  "/validateLogin",
-  (req, res, next) => {
+app.get('/validateLogin', (req, res, next) => {
 
     //If the token is valid, the logic extracts the user id and the role information.
     //If the role is not user, then response 403 UnAuthorized
